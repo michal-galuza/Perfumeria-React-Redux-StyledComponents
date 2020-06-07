9ed12351b54799dd081a46420313be85a127eba4
michal-galuza <63243104+michal-galuza@users.noreply.github.com>
2020-06-07 15:39:30 +0200

    Add files via upload

---
+++ productsForHimData.js
@@ -0,0 +1,74 @@
+//minimum 500x500 pxlewy dolny róg zdjęcia
+//bez napisów
+//otwierasz zdjecie lewym przyciskiem
+//prawy przycis myszy na zdjecie
+//koiuj adres obrazu
+//otwierasz cudzysłów i wklejasz
+//przecinek
+//name type  description price image
+//dla niego
+
+export const zapachy=[
+   {
+     name: "zapaszek cytrynowy",
+     type:"zapach",
+     description:"pachnie jak cytrynka",
+     price:"  2240",
+     image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"
+    },
+    {
+      name: "zapaszek do dupy",
+     description:"pachnie jak dupka",
+     price:"  40",
+     image:'  https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg'
+    },{
+      type:"scents",
+      for:"him",
+     name: "zapaszek pomarańczowy",
+     description:"pachnie jak męska pomarańczka",
+     price:"  140",
+     image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"
+    },{
+      name: "odświeżacz do toalety",
+      description:"pachnie jak męska wagina",
+      price:"  220",
+      image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"
+     },
+     {
+      name: "zapaszek pomarańczowy",
+      description:"pachnie jak męska pomarańczka",
+      price:"  140",
+      image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"
+     },
+     {
+      name: "zapaszek pomarańczowy",
+      description:"pachnie jak męska pomarańczka",
+      price:"  140",
+      image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"
+     },
+     {
+      name: "zapaszek pomarańczowy",
+      description:"pachnie jak męska pomarańczka",
+      price:"  140",
+      image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"
+     },
+];
+ export const krem_pod_oczy=[
+  {
+    name: "krem na noc ",
+    description:"kremuj tylko w nocy",
+    price:"  220",
+    image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"
+   },{
+     name: "kremik męski w dzień kremuj tylko w dzień",
+    description:"kremowanie niczym w aushwitz",
+    price:"  40",
+    image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"
+   },{
+    name: "kremik do dupy ",
+    description:"kremik na 15:30 aby rowek był zawsze niebieski",
+    price:"  bezcenne",
+    image:"https://image.ceneostatic.pl/data/products/97675/i-chanel-coco-perfumy-7-5ml.jpg"},
+];
+ export const krem_do_twarzy=[]
+ export const pielęgnacja_brody=[];
